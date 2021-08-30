39ed3e6f168f680469fb1d208e2998d170f6b541
Oscar Esgalha <oscar@instruct.com.br>
2021-08-30 10:56:43 -0300

    feat: update CI platform prompt
    Add a hint about the CLI usage, stating
    how to navigate through options. This is
    more relevant for Windows because the
    navigation control aren't the arrow keys.
    
    This also adds a control to ensure the user
    selects at least one CI platform.

--- src/cli/prelude/config.ts
+++ src/cli/prelude/config.ts
@@ -2,6 +2,20 @@ import { Checkbox, log } from "../../../deps.ts";
 import { config } from "../../config/mod.ts";
 import { isPlatform } from "../../platform/mod.ts";
 
+// TODO
+// The handling of some specific key presses on Windows doesn't
+// work correclty in Deno. As a workaround, the checkbox prompt
+// uses "u" and "d" to navigate across options. This hint informs
+// that to the user. We should remove this hint when the expected
+// behavior (up and down arrow keys) are available.
+//
+// Keep an eye on those issues:
+// https://github.com/c4spar/deno-cliffy/issues/272
+// https://github.com/denoland/deno/issues/5945
+const NAV_HINT = Deno.build.os === "windows"
+  ? "Press <space> to select, use 'u' and 'd' keys to navigate"
+  : "Press <space> to select, use arrow keys to navigate";
+
 export async function configure() {
   const logger = log.getLogger("main");
   // Load the project config if it exists
@@ -15,6 +29,8 @@ export async function configure() {
   logger.info("Configure this project");
   const ciPlatforms = (await Checkbox.prompt({
     message: "Which platforms does this project use?",
+    minOptions: 1, // The user must select at least one
+    hint: NAV_HINT,
     options: [
       { name: "GitHub Actions", value: "github", checked: true },
       { name: "GitLab CI (coming soon)", value: "gitlab", disabled: true },
