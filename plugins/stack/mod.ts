f91653a79746061cd03335b2bbb5bf207ba37c81
joao10lima <joao.nascimento@instruct.com.br>
2021-09-01 14:44:35 -0300

    feat: add html stack
    Adds prettier as a formatter for .html and Vue files.
    Uses ESLint and Stylelint for Vue linting, choosing ESLint as a default.
    
    Resolves: https://github.com/pipelinit/pipelinit-cli/issues/7

--- plugins/stack/mod.ts
+++ plugins/stack/mod.ts
@@ -1,20 +1,24 @@
 import { introspector as CssIntrospector } from "./css/mod.ts";
 import { introspector as JavaScriptIntrospector } from "./javascript/mod.ts";
 import { introspector as PythonIntrospector } from "./python/mod.ts";
+import { introspector as HtmlIntrospector } from "./html/mod.ts";
 
 import type CSSProject from "./css/mod.ts";
 import type JavaScriptProject from "./javascript/mod.ts";
 import type PythonProject from "./python/mod.ts";
+import type HtmlProject from "./html/mod.ts";
 
 export type ProjectData =
   | CSSProject
   | JavaScriptProject
-  | PythonProject;
+  | PythonProject
+  | HtmlProject;
 
-export type { CSSProject, JavaScriptProject, PythonProject };
+export type { CSSProject, HtmlProject, JavaScriptProject, PythonProject };
 
 export const introspectors = [
   { name: "css", ...CssIntrospector },
   { name: "javascript", ...JavaScriptIntrospector },
   { name: "python", ...PythonIntrospector },
+  { name: "html", ...HtmlIntrospector },
 ];
