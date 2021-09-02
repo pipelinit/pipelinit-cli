f1e0b05ede856c00911d82203f43a7c9e3c043d0
Oscar Esgalha <oscar@instruct.com.br>
2021-09-02 10:47:09 -0300

    chore: add test dependencies to core

---
+++ core/deps.ts
@@ -0,0 +1,3 @@
+// std
+export { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+export { deepMerge } from "https://deno.land/std@0.104.0/collections/mod.ts";
---
+++ core/tests/mod.ts
@@ -0,0 +1,1 @@
+export { assertEquals, deepMerge } from "../deps.ts";
