6bb94c2e0fdbd790b497580b885b35a4fbee1ec1
joao10lima <joao.nascimento@instruct.com.br>
2021-11-12 11:07:51 -0300

    chore: bump version

--- cli/tests/version_test.ts
+++ cli/tests/version_test.ts
@@ -3,6 +3,6 @@ import { output, test } from "./helpers.ts";
 
 test({ args: ["--version"] }, async (proc) => {
   const [stdout, _stderr, { code }] = await output(proc);
-  assertEquals(stdout, "0.2.0");
+  assertEquals(stdout, "0.2.1");
   assertEquals(code, 0);
 });
--- core/version.ts
+++ core/version.ts
@@ -1,1 +1,1 @@
-export const PIPELINIT_VERSION = "0.2.0";
+export const PIPELINIT_VERSION = "0.2.1";
