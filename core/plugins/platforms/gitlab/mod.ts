1590a6a89805c45347335954db60af86d0981971
joao10lima <joao.nascimento@instruct.com.br>
2022-01-19 13:32:43 -0300

    fix: adjust GitLab's generation
    When is detected more than one stack for GitLab CI
    The `gitlab-ci.yml` is overwritten by only one stack includes.
    
    This commit adds a generation of an `.gitlab-ci.yml`
    according to the generated stacks, with this when a new stack
    be added for GitLab is not necessary to define the file for the stack
    
    By default, the stages are defined as :
    ```
    stages:
      - <stack>-<stage>
    ```
    Now, in every stack is necessary to define the stage in the format above

--- core/plugins/platforms/gitlab/mod.ts
+++ core/plugins/platforms/gitlab/mod.ts
@@ -1,5 +1,37 @@
 import { PlatformWriterFn } from "../../../types.ts";
 
+type StageOrder = {
+  [key: string]: string[];
+};
+const stageOrder: StageOrder = {
+  "lint": [],
+  "format": [],
+  "sast": [],
+  "test": [],
+  "build": [],
+};
+
+/*
+Form a gitlab-ci.yml
+---
+# Generated with pipelinit 0.4.0
+# https://pipelinit.com/
+stages:
+  - lint
+include:
+  - local: ".gitlab-ci/pipelinit.python.lint.yaml"
+*/
+const gitlabCiTemplate = (
+  header: string,
+  stages: string,
+  includes: string,
+) =>
+  `${header}
+stages:
+${stages}
+include:
+${includes}`;
+
 export const gitlab: PlatformWriterFn = async (context, templates) => {
   const header = `
 # Generated with pipelinit ${context.version}
@@ -8,17 +40,27 @@ export const gitlab: PlatformWriterFn = async (context, templates) => {
   const configurationFiles = [];
   for await (const template of templates) {
     const { name, content } = template;
-    if (name.includes(".gitlab-ci")) {
-      configurationFiles.push({
-        path: ".gitlab-ci.yml",
-        content: `${header}${content}`,
-      });
-      continue;
-    }
+    // The default name for the template is:
+    // pipelinit.<stack>.<stage>
+    const [_, stack, stage] = name.split(".");
+
+    stageOrder[stage].push(`  - ${stack}-${stage}\n`);
     configurationFiles.push({
       path: `.gitlab-ci/${name}.yaml`,
       content: `${header}${content}`,
     });
   }
+
+  const stages = Object.values(stageOrder).map((stageText) => {
+    return stageText.join("");
+  });
+  const includes = Object.values(configurationFiles).map((includeName) => {
+    return `  - local: "${includeName.path}"\n`;
+  }).sort();
+
+  configurationFiles.push({
+    path: ".gitlab-ci.yml",
+    content: gitlabCiTemplate(header, stages.join(""), includes.join("")),
+  });
   return configurationFiles;
 };
